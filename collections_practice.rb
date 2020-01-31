378d9b5adcb0aaaba37dd8338499423a4f5c9449
Nicholas Petrone <nickpetrone92@gmail.com>
2020-01-31 21:12:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,8 +21,8 @@ end
 
 
 def swap_elements(array)
-  swap_elements_from_to(["a", "b", "c"],0,2)
-swap_elements_from_to(["a", "b", "c"],2,1)
+  
+  swap_elements_from_to(["a", "b", "c"],2,1)
   
 end
 
